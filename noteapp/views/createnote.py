b84e1d711ec861e660c2487092e4ab73912f2cef
Amany Siraj <amanysirajaldin@gmail.com>
2018-08-07 17:37:59 +0200

    return the note text

--- noteapp/views/createnote.py
+++ noteapp/views/createnote.py
@@ -3,16 +3,18 @@
 # http://flask.pocoo.org/docs/1.0/blueprints/
 
 from flask import Blueprint, render_template, request
-import sys
 
 cn = Blueprint('cn', __name__, template_folder='templates')
 
 @cn.route('/createnote', methods=['POST', 'GET'])
 def show():
-    # check the request method
     if request.method == 'POST':
-        # check if the submit button clicked
+    # check the request method
         if request.form.get('createnote_btn'):
-            return "Clicked!"
+        # check if the submit button clicked
+            text =  request.form.get('notetext')
+            # get the note text
+            return text
+            # return the note text
 
     return render_template("createnote.html")
