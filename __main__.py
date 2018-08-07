20209f7cae2f3923019625c6c02b76ee1ad8520d
Amany Siraj <amanysirajaldin@gmail.com>
2018-08-07 13:10:23 +0200

    Creating main code to run the app

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from noteapp.app import app
+
+# running on localhost
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ noteapp/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from noteapp.views.index import bp as index_bp
+
+app = Flask(__name__)
+
+# creatin a route to go to
+
+@app.route('/')
+def home():
+    return "Hello World"
