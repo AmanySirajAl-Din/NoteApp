fb02b17963361d7fd35d2c52f0d6d578b46940d7
Amany Siraj <amanysirajaldin@gmail.com>
2018-08-07 16:34:59 +0200

    handle the POST method

--- noteapp/views/createnote.py
+++ noteapp/views/createnote.py
@@ -9,4 +9,8 @@ cn = Blueprint('cn', __name__, template_folder='templates')
 
 @cn.route('/createnote', methods=['POST', 'GET'])
 def show():
+    if request.method == 'POST':
+        if request.form.get('createnote_btn'):
+            return "Clicked!"
+
     return render_template("createnote.html")
