be4b9e429f0020d10450d7977bd0f6d7caece987
Amany Siraj <amanysirajaldin@gmail.com>
2018-08-07 16:33:07 +0200

    Allow Post method and Fix input type

--- noteapp/views/createnote.py
+++ noteapp/views/createnote.py
@@ -2,10 +2,11 @@
 # flask blue print
 # http://flask.pocoo.org/docs/1.0/blueprints/
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import sys
 
 cn = Blueprint('cn', __name__, template_folder='templates')
 
-@cn.route('/createnote')
+@cn.route('/createnote', methods=['POST', 'GET'])
 def show():
     return render_template("createnote.html")
